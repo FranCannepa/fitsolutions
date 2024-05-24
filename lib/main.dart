1081c96d3436b852e4a4a8dea9432c6e42086498
Ryan Rocha <ryanrocha1311@gmail.com>
2024-05-23 23:23:01 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,16 @@
-
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fitsolutions/Modelo/UserData.dart';
+import 'package:fitsolutions/Screens/Dietas/dietas_screen.dart';
+import 'package:fitsolutions/Screens/Membresia/membresia_screen.dart';
+import 'package:fitsolutions/Screens/Registro/registro_screen.dart';
+import 'package:fitsolutions/Utilities/NavigatorService.dart';
+import 'package:flutter/material.dart';
+import 'package:fitsolutions/Screens/Home/home_screen.dart';
+import 'package:fitsolutions/Screens/Login/login_screen.dart';
+import 'package:fitsolutions/Screens/Profile/perfil_screen.dart';
+import 'package:fitsolutions/Theme/light_theme.dart';
+import 'package:fitsolutions/firebase_options.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
